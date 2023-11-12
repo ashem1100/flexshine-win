5f2ffdb483103b0c738702395a8bc6f2cdd65047
ashkanemami05 <ashkanemami2005@gmail.com>
2023-11-12 14:41:59 +0330

    mm1

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 
-    int n;
+   /* int n;
     int sumi=0, a;
     for (n = 0;n < 10;n++) {
         cout << "Enter number " << n + 1 << "Integer :";
@@ -12,7 +12,28 @@ int main()
     }
 
     cout << sumi << " is the sum!\n";
-    cout << sumi / n;
+    cout << sumi / n; */
+
+
+    /*float five = 5.0;
+    for (five;five <= 10.1;five += 0.1) {
+        cout << five << "\n";
+
+    }*/
+
+    int code;
+    for (char i = 'a'; i <= 'z'; i++) {
+        code = i;
+        cout << i << "\t" << code << endl;
+    }
+
+    char code;
+    for (int i = 0; i <256; i++) {
+        code = i;
+        cout << i << "\t" << code << endl;
+    }
 
 }
+
+
 
